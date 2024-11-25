60a009ea3c9b866fc706716f62836f35ba5b8ae5
ZLKCyber <88219725+Zlkcyber@users.noreply.github.com>
2024-11-25 16:55:16 +0800

    restart mine if claim success

--- utils/script.js
+++ utils/script.js
@@ -9,7 +9,7 @@ async function getMiningData(apiClient, extensionId) {
             const miningData = response.data.data;
 
             updateProgress(extensionId, miningData); 
-            updateMiningPoint(extensionId, miningData); 
+            await updateMiningPoint(extensionId, miningData, apiClient); 
 
             if (miningData.ended === 1) {
                 logger(`[${extensionId}] Mining has ended. Proceeding to claim mining points.`, 'debug');
@@ -21,12 +21,12 @@ async function getMiningData(apiClient, extensionId) {
     }
 }
 
-function updateMiningPoint(extensionId, miningData) {
+async function updateMiningPoint(extensionId, miningData, apiClient) {
     const elapsedTimeInHours = (Date.now() - new Date(miningData.start).getTime() - miningData.miss) / 36e5;
     const points = elapsedTimeInHours * miningData.hourly;
     const miningPoint = Math.max(0, points);
-
-    logger(`[${extensionId}] Points: ${points}, MiningPoints: ${miningPoint}, ElapsedTimeInHours: ${elapsedTimeInHours}`, 'warn');
+    const totalPoints = await checkBalance(apiClient, extensionId)
+    logger(`[${extensionId}] Total Points: ${totalPoints}, MiningPoints: ${miningPoint}, ElapsedTimeInHours: ${elapsedTimeInHours}`, 'warn');
 }
 
 function updateProgress(extensionId, miningData) {
@@ -46,8 +46,51 @@ async function claim(apiClient, extensionId) {
         logger(`[${extensionId}] Claiming mining points...`);
         const { data } = await apiClient.post('/mining/claim', { extension: extensionId });
         logger(`[${extensionId}] Claimed successfully:`, 'success', data);
+        await startFarming(apiClient, extensionId);
     } catch (error) {
         logger(`[${extensionId}] Error during claim:`, 'error', error.message || error);
+        //logger(`[${extensionId}] Restarting farming...`)
+        //startFarming(apiClient, extensionId);
     }
 }
+
+async function startFarming(apiClient, extensionId) {
+    try {
+        const response = await apiClient.post('/mining/start', {
+            extension: extensionId
+        });
+        if (response.status === 200) {
+            logger(`[${extensionId}] Mining started successfully`, 'success');
+        }
+    } catch (error) {
+        if (error.response) {
+            const { status, data } = error.response;
+            logger(`[${extensionId}] Error starting mining (HTTP Error):`, 'error', {
+                status,
+                data
+            });
+
+            // Check if mining is already started 
+            if (status === 412 && data.error.message === 'Mining is started.') {
+                console.log(`[${extensionId}] Mining already started. Skipping start process.`);
+                return; 
+            }
+        } else {
+            logger(`[${extensionId}] Error starting mining:`, 'error', error.message || error);
+        }
+    }
+}
+async function checkBalance(apiClient, extensionId) {
+    try {
+        logger(`[${extensionId}] Checking Balances points...`)
+        const response = await apiClient.get('/user/balances');
+        const balances = response.data.data[0].balance;
+        logger(`[${extensionId}] Balances:`, 'info', balances);
+        return balances;
+    } catch (error) { 
+        logger(`[${extensionId}] Error checking balance:`, 'error', error.message || error);
+        return null;
+    }
+}
+    
 export { getMiningData };
